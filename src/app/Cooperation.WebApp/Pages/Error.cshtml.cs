89dec6c49355ceaa312651026456efa6679a5a68
Victoria Kondratieva <victoria.kondratieva3@gmail.com>
2023-05-19 14:04:46 +0300

    #19 move project to src folder

--- src/app/Cooperation.WebApp/Pages/Error.cshtml.cs
+++ src/app/Cooperation.WebApp/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Cooperation.Pages
+namespace Cooperation.WebApp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- src/app/Cooperation.WebApp/Pages/Privacy.cshtml.cs
+++ src/app/Cooperation.WebApp/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Cooperation.Pages
+namespace Cooperation.WebApp.Pages
 {
     public class PrivacyModel : PageModel
     {
